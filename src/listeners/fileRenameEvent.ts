bbc97be53bd9da5da9fc99a93897e669be6c1787
rarula <74240663+rarula@users.noreply.github.com>
2023-09-06 12:27:40 +0900

    🐛 ファイルをリネームした時に構成ファイルが生成される問題を修正

--- src/listeners/fileRenameEvent.ts
+++ src/listeners/fileRenameEvent.ts
@@ -1,24 +1,33 @@
+import { existsSync } from 'fs-extra';
 import { FileRenameEvent, workspace } from 'vscode';
 
 import { Workspace } from '../workspace';
 
 export function onDidFileRename(event: FileRenameEvent, workspaceInstance: Workspace): void {
-    event.files.forEach((file) => {
-        const oldRelativePath = workspace.asRelativePath(file.oldUri, false);
-        const newRelativePath = workspace.asRelativePath(file.newUri, false);
-
+    if (existsSync(workspaceInstance.configFilePath)) {
         const taskMap = workspaceInstance.getConfiguration().taskMap;
 
-        for (const key in taskMap) {
-            if (key.startsWith(oldRelativePath)) {
-                const fixedKey = key.replace(oldRelativePath, newRelativePath);
+        let modified = false;
 
-                taskMap[fixedKey] = taskMap[key];
-                delete taskMap[key];
+        event.files.forEach((file) => {
+            const oldRelativePath = workspace.asRelativePath(file.oldUri, false);
+            const newRelativePath = workspace.asRelativePath(file.newUri, false);
+
+            for (const key in taskMap) {
+                if (key.startsWith(oldRelativePath)) {
+                    const fixedKey = key.replace(oldRelativePath, newRelativePath);
+
+                    taskMap[fixedKey] = taskMap[key];
+                    delete taskMap[key];
+
+                    modified = true;
+                }
             }
-        }
+        });
 
-        workspaceInstance.updateTaskMap(taskMap);
-        workspaceInstance.decorationProvider.decorate(taskMap);
-    });
+        if (modified) {
+            workspaceInstance.updateTaskMap(taskMap);
+            workspaceInstance.decorationProvider.decorate(taskMap);
+        }
+    }
 }
